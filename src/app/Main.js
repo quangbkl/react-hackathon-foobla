dfb5c1e05f74a403c6e40a4ac79975658fd6cb64
quangbkl <quang99dev@gmail.com>
2018-11-09 00:24:38 +0700

    add router

---
+++ src/app/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom'
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route/>
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Main;
